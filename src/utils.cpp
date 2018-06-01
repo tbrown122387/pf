b0706e71e5039a98b9e1668bfdba62247b6db9ea
Taylor <tbrown122387@gmail.com>
2018-06-01 13:20:57 -0400

    change format to characters that don't have to be escaped

--- src/utils.cpp
+++ src/utils.cpp
@@ -9,7 +9,8 @@ std::string utils::genStringWithTime(const std::string& str) {
     tstruct = *localtime(&now);
     // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
     // for more information about date/time format
-    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+//    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%H-%M-%S", &tstruct);
     
     return str + "_" + buf;
 }
