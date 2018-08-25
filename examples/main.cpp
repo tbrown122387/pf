d8f16861a4db5817e2aafa68ee11e3f67d382a55
Taylor <trb5me@virginia.edu>
2018-08-24 21:54:23 -0400

    make example easier

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,8 +3,12 @@
 
 int main(int argc, char **argv)
 {
-    
-    run_svol_comparison("/home/taylor/ssm_examples/data/some_csvs/svol_y_data.csv");
+	if(argc == 1){
+		printf("please enter the path to the data file");
+	}else{
+		run_svol_comparison(argv[1]);
+	    //run_svol_comparison("/home/taylor/ssm_examples/data/some_csvs/svol_y_data.csv");
 
+	}
 	return 0;
 }
