944d9895ee23b968d9b9a2c505978f74bfa5dd25
Taylor <tbrown122387@gmail.com>
2018-11-12 23:27:15 -0500

    add binom coeff func

--- src/rv_eval.cpp
+++ src/rv_eval.cpp
@@ -52,6 +52,21 @@ double rveval::log_inv_logit(const double& r)
 }
 
 
+unsigned int sf::nChoosek( unsigned int n, unsigned int k )
+{
+    if (k > n) return 0;
+        if (k * 2 > n) /*return*/ k = n-k;  //remove the commented section
+        if (k == 0) return 1;
+
+    int result = n;
+    for( int i = 2; i <= k; ++i ) {
+        result *= (n-i+1);
+        result /= i;
+    }
+    return result;
+}
+
+
 double rveval::evalUnivNorm(const double &x, const double &mu, const double &sigma, bool log)
 {
     double exponent = -.5*(x - mu)*(x-mu)/(sigma*sigma);
