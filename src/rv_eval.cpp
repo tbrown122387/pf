19d2d1eb93fdd25a9001a6c5a6a4b506ad308b80
Taylor <tbrown122387@gmail.com>
2018-11-12 23:32:02 -0500

    woops, last one was too fast

--- src/rv_eval.cpp
+++ src/rv_eval.cpp
@@ -52,11 +52,11 @@ double rveval::log_inv_logit(const double& r)
 }
 
 
-unsigned int sf::nChoosek( unsigned int n, unsigned int k )
+unsigned int rveval::nChooseK( unsigned int n, unsigned int k )
 {
     if (k > n) return 0;
-        if (k * 2 > n) /*return*/ k = n-k;  //remove the commented section
-        if (k == 0) return 1;
+    if (k * 2 > n) /*return*/ k = n-k;  //remove the commented section
+    if (k == 0) return 1;
 
     int result = n;
     for( int i = 2; i <= k; ++i ) {
