e3e9e9851392ddb22adba6126ec9a5c552743d36
Taylor <trb5me@virginia.edu>
2020-09-30 21:03:01 -0400

    first draft of smctc comparison example

---
+++ smctc_comparison_example/acv_bs.h
@@ -0,0 +1,121 @@
+#ifndef ACV_BS_H
+#define ACV_BS_H
+
+
+#include <Eigen/Dense>
+
+#include <pf/bootstrap_filter.h> // the boostrap particle filter
+#include <pf/rv_samp.h> // for sampling random numbers
+#include <pf/rv_eval.h> // for evaluating densities and pmfs
+
+
+template<size_t nparts, size_t dimx, size_t dimy, typename resampT, typename float_t>
+class acv_bs : public BSFilter<nparts, dimx, dimy, resampT, float_t>
+{
+public:
+    using ssv = Eigen::Matrix<float_t, dimx, 1>;
+    using osv = Eigen::Matrix<float_t, dimy, 1>;
+    using ssm = Eigen::Matrix<float_t, dimx, dimx>;
+    using osm = Eigen::Matrix<float_t, dimy, dimy>;
+    using esm = Eigen::Matrix<float_t, dimy,dimx>; // emission sized matrix
+
+    // parameters (TODO remove some)
+    float_t m_var_s0;
+    float_t m_var_u0;
+//    float_t m_var_s;
+//    float_t m_var_u;
+//    float_t m_scale_y;
+    float_t m_nu_y;
+//    float_t Delta;
+
+    // higher-level parameters
+    //ssm m_state_error_cov;
+    ssm m_A;
+    esm m_B;
+    osm m_obs_shape_mat;
+
+    // use this for samplign
+    rvsamp::UnivNormSampler<float_t> m_stdNormSampler; // for sampling 
+    rvsamp::UnivStudTSampler<float_t> m_t_sampler;
+    rvsamp::MVNSampler<dimx,float_t> m_state_error_sampler;
+
+    // ctor
+    acv_bs(float_t var_s0, float_t var_u0, float_t var_s, float_t var_u, float_t scale_y, float_t nu_y, float_t Delta);
+    
+    // required by bootstrap filter base class
+    float_t logQ1Ev(const ssv &x1, const osv &y1);
+    float_t logMuEv(const ssv &x1);
+    float_t logGEv(const osv &yt, const ssv &xt);
+    auto fSamp(const ssv &xtm1) -> ssv;
+    auto q1Samp(const osv &y1) -> ssv;
+};
+
+
+template<size_t nparts, size_t dimx, size_t dimy, typename resampT, typename float_t>
+acv_bs<nparts, dimx, dimy, resampT, float_t>::acv_bs( float_t var_s0, float_t var_u0, float_t var_s, float_t var_u, float_t scale_y, float_t nu_y, float_t Delta)
+    : m_var_s0(var_s0), m_var_u0(var_u0), m_nu_y(nu_y)
+    , m_A(ssm::Identity()), m_B(esm::Zero()), m_obs_shape_mat(scale_y * osm::Identity())
+    , m_t_sampler(nu_y), m_state_error_sampler(ssv::Zero(), ssm::Identity()) 
+{
+    ssm state_error_cov(ssm::Zero()); 
+    state_error_cov(0,0) = var_s;
+    state_error_cov(1,1) = var_u;
+    state_error_cov(2,2) = var_s;
+    state_error_cov(3,3) = var_u;
+    m_state_error_sampler.setCovar(state_error_cov);
+
+    m_A(0,1) = Delta;
+    m_A(2,3) = Delta;
+
+    m_B(0,0) = 1.0;
+    m_B(1,2) = 1.0;
+}
+
+
+template<size_t nparts, size_t dimx, size_t dimy, typename resampT, typename float_t>
+auto acv_bs<nparts, dimx, dimy, resampT, float_t>::q1Samp(const osv &y1) -> ssv
+{
+    ssv x1samp;
+    x1samp(0) = m_stdNormSampler.sample() * std::sqrt(m_var_s0);
+    x1samp(1) = m_stdNormSampler.sample() * std::sqrt(m_var_u0);
+    x1samp(2) = m_stdNormSampler.sample() * std::sqrt(m_var_s0);
+    x1samp(3) = m_stdNormSampler.sample() * std::sqrt(m_var_u0);
+    return x1samp;
+}
+
+
+template<size_t nparts, size_t dimx, size_t dimy, typename resampT, typename float_t>
+auto acv_bs<nparts, dimx, dimy, resampT, float_t>::fSamp(const ssv &xtm1) -> ssv
+{
+    return m_A * xtm1 + m_state_error_sampler.sample(); 
+}
+
+
+template<size_t nparts, size_t dimx, size_t dimy, typename resampT, typename float_t>
+float_t acv_bs<nparts, dimx, dimy, resampT, float_t>::logGEv(const osv &yt, const ssv &xt)
+{
+    return rveval::evalMultivT<dimy,float_t>(yt, m_B * xt, m_obs_shape_mat, m_nu_y, true);
+}
+
+
+template<size_t nparts, size_t dimx, size_t dimy, typename resampT, typename float_t>
+float_t acv_bs<nparts, dimx, dimy, resampT, float_t>::logMuEv(const ssv &x1)
+{
+    return rveval::evalUnivNorm<float_t>(x1(0),0.0,std::sqrt(m_var_s0), true) + 
+           rveval::evalUnivNorm<float_t>(x1(1),0.0,std::sqrt(m_var_u0), true) + 
+           rveval::evalUnivNorm<float_t>(x1(2),0.0,std::sqrt(m_var_s0), true) + 
+           rveval::evalUnivNorm<float_t>(x1(3),0.0,std::sqrt(m_var_u0), true); 
+}
+
+
+template<size_t nparts, size_t dimx, size_t dimy, typename resampT, typename float_t>
+float_t acv_bs<nparts, dimx, dimy, resampT, float_t>::logQ1Ev(const ssv &x1samp, const osv &y1)
+{
+    return rveval::evalUnivNorm<float_t>(x1samp(0),0.0,std::sqrt(m_var_s0), true) + 
+           rveval::evalUnivNorm<float_t>(x1samp(1),0.0,std::sqrt(m_var_u0), true) + 
+           rveval::evalUnivNorm<float_t>(x1samp(2),0.0,std::sqrt(m_var_s0), true) + 
+           rveval::evalUnivNorm<float_t>(x1samp(3),0.0,std::sqrt(m_var_u0), true); 
+}
+
+
+#endif //ACV_BS_H
---
+++ smctc_comparison_example/main.cpp
@@ -0,0 +1,62 @@
+#include <fstream> // for ifstream
+
+#include <pf/resamplers.h> 
+
+#include "data_reader.h"
+#include "acv_bs.h" // bs filter for svol
+
+// some template parameters
+#define dimstate 4 
+#define dimobs   2
+#define numparts 1000
+#define FLOATTYPE float // choose float (faster) or double (slower)
+
+
+int main(int argc, char** argv)
+{
+
+    // choose your resampler
+    //using multinomialR = mn_resampler        <numparts,dimstate,FLOATTYPE>;
+    using fastMultinomR= mn_resamp_fast1<numparts,dimstate,FLOATTYPE>;
+
+    // model parameters 
+    FLOATTYPE var_s0 = 4.0;
+    FLOATTYPE var_u0 = 1.0;
+    FLOATTYPE var_s = .02;
+    FLOATTYPE var_u = .001;
+    FLOATTYPE scale_y = .1;
+    FLOATTYPE nu_y = 10.0;
+    FLOATTYPE Delta = .1;
+
+    // instantiate a boostrap filter almost constant variance model object
+    acv_bs<numparts, dimstate, dimobs, fastMultinomR,FLOATTYPE> 
+        mod(var_s0, var_u0, var_s, var_u, scale_y, nu_y, Delta);
+
+    // read in some data
+    using osv = Eigen::Matrix<FLOATTYPE,dimobs,1>;   
+    std::vector<osv> data = readInData<FLOATTYPE,dimobs>("data.csv", ' ');
+
+    // optional lambda for filtering expectation approximations 
+    using ssv = Eigen::Matrix<FLOATTYPE,dimstate,1>;
+    using Mat = Eigen::Matrix<FLOATTYPE,Eigen::Dynamic,Eigen::Dynamic>;    // at each time point, which approximates the 
+    auto idtyLambda = [](const ssv& xt) -> const Mat  
+    {
+        return xt;
+    };
+    std::vector<std::function<const Mat(const ssv&)>> v;
+    v.push_back(idtyLambda);
+
+    // iterate over the data (finally)
+    // printing stuff is obviously optional
+    for(size_t row = 0; row < data.size(); ++row){
+        mod.filter(data[row], v); 
+        std::cout << mod.getExpectations()[0] << "\n";
+//        std::cout << bssvol1.getExpectations()[0] << ", ";
+//        std::cout << bssvol2.getExpectations()[0] << ", ";
+//        std::cout << bssvol3.getExpectations()[0] << ", ";
+//        std::cout << bssvol4.getExpectations()[0] << ", ";
+//        std::cout << bssvol5.getExpectations()[0] << "\n";
+    }
+   
+   return 1; 
+}
